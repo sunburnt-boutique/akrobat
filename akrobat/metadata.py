e5214b2c02138be8197236dca75678fdf31c0952
Sunburnt Boutique <sunburnt-boutique@users.noreply.github.com>
2016-08-02 21:04:04 -0400

    Set up akrobat project.

---
+++ akrobat/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'akrobat'
+project = 'AkrobatModule'
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Learners for OpenAI Gym'
+authors = ['Sunburnt Boutique']
+authors_string = ', '.join(authors)
+emails = ['sunburnt-boutique@users.noreply.github.com']
+license = 'MIT'
+copyright = '2016 ' + authors_string
+url = 'https://github.com/sunburnt-boutique/q'
